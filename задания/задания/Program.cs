a2bd49380d4e21ddcda8cdf2816a2704c3d88694
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2022-09-28 21:50:19 +0300

    Add files via upload

---
+++ задания/задания/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class MainClass
+{
+
+    public static void Main(string[] args) {
+        double a, b;
+        Console.WriteLine("Введите A");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите B");
+        b = Convert.ToInt32(Console.ReadLine());
+        if (b == 0)
+            Console.WriteLine("Корень = {0}", -b / a);
+        else if (b > 0)
+            Console.WriteLine("Корни = {0}", (b / a, -b / a));
+        else
+            Console.WriteLine("Нет решений");
+    }
+}
